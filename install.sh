31121ef3d7ed3163c73594b3ed4b1ee57f3addb7
moenka <moenka@10forge.org>
2018-04-10 14:44:54 +0200

    Add terminator config, remove cli args from install script since seldome used since creation

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-# Install into the $HOME directory.
+# Install contents of this repository into the $HOME directory.
+
 DIR=$(dirname "$(readlink -e "$0")")
 IGNORE=( -I . -I .. -I doc -I .git -I .gitignore -I .gitconfig -I install.sh -I README.md)
-case $1 in
-    f|full)
-        echo "Installing full version..."
-        ls --color=none -a ${IGNORE[*]} $DIR | xargs -i cp -r $DIR/{} $HOME
-        ;;
-    g|git)
-        cp .gitconfig $HOME
-        ;;
-    *)
-        echo "Argument missing. Specify m(in) or f(ull) or just g(it) version."
-        ;;
-esac
+
+echo -n "Installing into ${HOME} ... "
+ls --color=none -a ${IGNORE[*]} $DIR | xargs -i cp -r $DIR/{} $HOME && echo "done" || echo "failed"
+
